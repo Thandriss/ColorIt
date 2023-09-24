501d1abea5b371adb11167db45c43b9cb5292c11
Thandriss <mahonko_yana@mail.ru>
2023-09-24 19:26:02 +0300

    Layput draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,190 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="background">
+            <div className="leftBar">
+                <div className="objectsBar">
+                    <ul className="listOfObjects">
+                        <table>
+                            <thead>
+                            Objects
+                            </thead>
+
+                            <tbody align="center">
+                            <tr>
+                                <td>blablaидфd</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            </tbody>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff"></input></td>
+                            </tr>
+                            <tr>
+                                <td>blabla</td>
+                                <td><input type ="text" value="#ffffff" ></input></td>
+                            </tr>
+                            <tfoot>
+                            <tr>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </ul>
+                </div>
+                <button className="applyButton">Apply</button>
+                <div className="colorBar">
+                    <table>
+                        <thead>
+                        Colors
+                        </thead>
+
+                        <tbody align="center">
+                        <tr>
+                            <td><div className="colorField"></div></td>
+                            <td><div className="colorField"></div></td>
+                            <td><div className="colorField"></div></td>
+                            <td><div className="colorField"></div></td>
+                            <td><div className="colorField"></div></td>
+                        </tr>
+                        </tbody>
+                        <tr>
+                            <td className="colorNum">text</td>
+                            <td className="colorNum">text</td>
+                            <td className="colorNum">text</td>
+                            <td className="colorNum">text</td>
+                            <td className="colorNum">text</td>
+                        </tr>
+                        <tfoot>
+                        <tr>
+                        </tr>
+                        </tfoot>
+                    </table> <div></div>
+                </div>
+                <button className="genButton">Generate</button>
+            </div>
+            <div className="rightBar">
+            </div>
+        </div>
+
+      {/*<div>*/}
+        {/*<a href="https://vitejs.dev" target="_blank">*/}
+        {/*  <img src={viteLogo} className="logo" alt="Vite logo" />*/}
+        {/*</a>*/}
+        {/*<a href="https://react.dev" target="_blank">*/}
+        {/*  <img src={reactLogo} className="logo react" alt="React logo" />*/}
+        {/*</a>*/}
+      {/*</div>*/}
+      {/*<h1>Vite + React</h1>*/}
+      {/*<div className="card">*/}
+      {/*  <button onClick={() => setCount((count) => count + 1)}>*/}
+      {/*    count is {count}*/}
+      {/*  </button>*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.jsx</code> and save to test HMR*/}
+      {/*  </p>*/}
+      {/*</div>*/}
+      {/*<p className="read-the-docs">*/}
+      {/*  Click on the Vite and React logos to learn more*/}
+      {/*</p>*/}
     </>
   )
 }
